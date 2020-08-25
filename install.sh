1b59abeed9c16cc175a1f096a36c9f3a588d46dd
Jeremiah Boyle <jeremiah.boyle@gmail.com>
2020-08-25 10:54:45 -0700

    Capture git hash of installed version

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ cp VFF.vim ~/.vim/plugin/
 cp VFF.rb ~/.vim/plugin/VFF
 cp bin/release/*.exe ~/.vim/plugin/VFF
 cp bin/release/*.dll ~/.vim/plugin/VFF
+hash=`git rev-parse --short HEAD`
+if [ ! -z "$hash" ]; then
+    echo $hash > ~/.vim/plugin/VFF/version
+fi
